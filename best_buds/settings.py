7ce4d1520c1a546702646fbe3b9d846ea89a6429
Troxed <116315838+Troxed@users.noreply.github.com>
2022-12-06 21:00:23 -0500

    changed from envrion to python-decouple

--- best_buds/settings.py
+++ best_buds/settings.py
@@ -11,13 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
-import environ
 
-env = environ.Env()
 
-# read th .env file
-environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['bestbuds420.herokuapp.com']
 
