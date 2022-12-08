5cfc7086be1d32a83a29fedd44f8e5ab2fe24fc2
Troxed <116315838+Troxed@users.noreply.github.com>
2022-12-07 19:09:40 -0500

    add dj_database_url import in settings.py

--- best_buds/settings.py
+++ best_buds/settings.py
@@ -114,7 +114,7 @@ DATABASES = {
 }
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES["default"].udate(db_from_env)
+DATABASES["default"].update(db_from_env)
 
 
 
