1ece3b90d9181ea1e84e2f1e130d51989db30879
Troxed <116315838+Troxed@users.noreply.github.com>
2022-12-08 16:35:10 -0500

    removed static files storage variable

--- best_buds/settings.py
+++ best_buds/settings.py
@@ -127,7 +127,7 @@ AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
 
 
 # Password validation
