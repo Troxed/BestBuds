da7e60ad93bb21f15a6cef168a16178e54c01582
Troxed <116315838+Troxed@users.noreply.github.com>
2022-12-07 19:13:15 -0500

    add dj_database_url import in settings.py

--- best_buds/settings.py
+++ best_buds/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
-
+import dj_database_url
 
 
 
@@ -112,7 +112,6 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES["default"].update(db_from_env)
 
