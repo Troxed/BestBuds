9fee51f2ee18a9c9920d808d7258645154222d2e
Troxed <116315838+Troxed@users.noreply.github.com>
2022-12-10 16:39:54 -0500

    testing DB setting

--- best_buds/settings.py
+++ best_buds/settings.py
@@ -100,23 +100,23 @@ WSGI_APPLICATION = "best_buds.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "railway",
-#         "USER": "postgres",
-#         "PASSWORD": "u44iuFZHluBDV7yGLPeu",
-#         "HOST": "containers-us-west-160.railway.app",
-#         "PORT": 7764,
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "u44iuFZHluBDV7yGLPeu",
+        "HOST": "containers-us-west-160.railway.app",
+        "PORT": 7764,
+    }
+}
 
 # Amazon S3 bucket config
 
