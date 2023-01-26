c7d10c4c1b7b44111438958c90d420ef38258fc5
Troxed <116315838+Troxed@users.noreply.github.com>
2023-01-26 13:08:37 -0500

    Changed url path in concentrats\views.py

--- concentrates/views.py
+++ concentrates/views.py
@@ -6,7 +6,7 @@ from .models import Concentrate
 def concentrate_list(request):
     concentrates = Concentrate.objects.all().order_by('name')
 
-    return render(request, "concentrates/concentrates_list.html", {
+    return render(request, "concentrates/concentrate_list.html", {
         'concentrates': concentrates,
     })
 
@@ -14,7 +14,7 @@ def concentrate_list(request):
 def concentrate_detail(request, pk):
     concentrate = get_object_or_404(Concentrate, pk=pk)
 
-    return render(request, "concentrates/concentrates_detail.html", {
+    return render(request, "concentrates/concentrate_detail.html", {
         'concentrate': concentrate
     })
 
