fcfdd4f9fa82ff4f56cd67cd5cbe84ee551e3563
Troxed <116315838+Troxed@users.noreply.github.com>
2023-01-26 13:01:52 -0500

    Registered concentrate model in admin.py

--- concentrates/admin.py
+++ concentrates/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Concentrate
+
+admin.site.register(Concentrate)
--- concentrates/models.py
+++ concentrates/models.py
@@ -4,3 +4,6 @@ class Concentrate(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     image = models.ImageField(upload_to='concentrates/img')
+
+    def __str__(self):
+        return self.name
